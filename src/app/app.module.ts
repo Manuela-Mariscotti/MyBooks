6cfd62784ca30871414c3fcf353c9238efd9a02c
Manuela-Mariscotti <manuela.mariscotti@gmail.com>
2022-08-31 11:28:11 +0200

    pagina libros y clase libro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { FormularioRegistroComponent } from './component/formulario-registro/formulario-registro.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
+import { LibrosComponent } from './pages/libros/libros.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { PerfilComponent } from './pages/perfil/perfil.component';
     HomeComponent,
     FormularioRegistroComponent,
     RegistroComponent,
-    PerfilComponent
+    PerfilComponent,
+    LibrosComponent
   ],
   imports: [
     BrowserModule,
--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -8,7 +8,7 @@ export class Usuario {
 
     
     constructor(id_usuario:number,nombre:string, apellido:string, email:string, url:string, password:string){
-        this.id_usuario = id_usuario
+        this.id_usuario = id_usuario;
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
